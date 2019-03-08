338565dd8c1047a99b315cc1511dcffca71f77e2
Diego Camargo <dicamargov@gmail.com>
2019-03-08 06:37:12 +0100

    Add the capabilitites to also search by longitude

--- src/terminal_lookup/business_logic/find_nearby_terminals.rb
+++ src/terminal_lookup/business_logic/find_nearby_terminals.rb
@@ -8,6 +8,8 @@ module TerminalLookup
     ##
     # Finds nearby terminals
     class FindNearbyTerminals
+      LOGGER = ::Logger.new(STDOUT, level: Config.log.level)
+
       def initialize(address:, limit: 10, radius: 50_000)
         @address = address
         @limit = limit
@@ -15,10 +17,22 @@ module TerminalLookup
       end
 
       def run
-        point = Repository::Geocoding.search(address: @address)
+        point = coordinates
         return [] unless point
 
+        LOGGER.info("Searching for #{point}")
+
         Repository::Location.closest_locations(point: point, radius: @radius, limit: @limit)
+      end
+
+      private
+
+      def coordinates
+        latitude, longitude = @address.split(',').map { |e| Float(e) }.first(2)
+        { latitude: latitude, longitude: longitude }
+      rescue ArgumentError, TypeError
+        LOGGER.info("Searching for #{@address}")
+        Repository::Geocoding.search(address: @address)
       end
     end
   end
